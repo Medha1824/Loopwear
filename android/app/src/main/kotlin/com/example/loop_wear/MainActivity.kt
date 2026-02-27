0353945ed43c7591724a3d33630eaf6548016e1e
Tamanna Akter <medha18tam@gmail.com>
2026-02-28 00:34:29 +0600

    Beginning of project

---
+++ android/app/src/main/kotlin/com/example/loop_wear/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.loop_wear
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'loopwear.com',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.cyan),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            Text('You have pushed the button this many times:'),
+          ],
+        ),
+      ),
+    );
+  }
+}
