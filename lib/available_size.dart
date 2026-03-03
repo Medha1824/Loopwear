0c0a75a73fa604589bfe4cb2c4abedf09fe534f1
Tamanna Akter <medha18tam@gmail.com>
2026-03-03 22:22:06 +0600

    Product Details Page update

---
+++ lib/available_size.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AvailableSize extends StatefulWidget{
+  final String size;
+  const AvailableSize({super.key, required this.size});
+
+  @override
+  State<AvailableSize> createState() => _AvailableSizeState();
+}
+
+class _AvailableSizeState extends State<AvailableSize> {
+  bool isSelected=false;
+  @override
+  Widget build(BuildContext context) => GestureDetector(
+    onTap: (){
+      setState((){
+        isSelected=!isSelected;
+      });
+    },
+    child: Container(
+      margin:EdgeInsets.only(right:16),
+      width:40,
+      height:30,
+        decoration:BoxDecoration(
+          color:isSelected? Color(0xFFCCB7AE): Colors.transparent,
+            borderRadius: BorderRadius.circular(6),
+          border:Border.all(color:Color(0xFFCCB7AE)),
+        ),
+    child:Center(
+      child: Text(widget.size,style:TextStyle(
+        fontWeight: FontWeight.bold,
+        color: Color(0xFF65514C),
+        fontSize: 16,
+      )),
+    ),
+    ),
+  );
+}
